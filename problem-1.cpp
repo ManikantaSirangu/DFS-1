6ce21f29ba44c95e899af85564c314f90029d2ec
ManikantaSirangu <Sirangubalu21@gmail.com>
2024-02-28 07:08:44 +0530

    Done DFS-1

---
+++ problem-1.cpp
@@ -0,0 +1,39 @@
+// t-c: m*n;
+// s-c: m*n;
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+        int m = image.size(), n = image[0].size(), nr, nc;
+        if(image[sr][sc] == color)
+            return image;
+        int nw = image[sr][sc];
+        image[sr][sc] = color;
+        queue<int> q;
+        q.push(sr);
+        q.push(sc);
+        while(!q.empty())
+        {
+            sr = q.front();
+            q.pop();
+            sc = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++)
+            {
+                nr = sr + dirs[i][0];
+                nc = sc + dirs[i][1];
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n)
+                {
+                    if(image[nr][nc] == nw)
+                    {
+                        image[nr][nc] = color;
+                        q.push(nr);
+                        q.push(nc);
+                    }
+                }
+            }
+        }
+        return image;
+
+    }
+};
